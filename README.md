9b1ff675ffb87891fe3953e331b4ef88b2521700
Aron Duby <aron.duby@gmail.com>
2016-07-10 01:11:31 -0400

    She's alive!

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # dibsServer
-server for spOctopus
+node/redis backend server for [spOctopus](https://github.com/aronduby/spOctopus)
+
+## Installation
+Once you've downloaded/cloned you mostly just have to create the config file. `config.example.json` is included as a starting point. Here's a breakdown of the different options.
+ 
+ Setting|Type|Description
+ ---|---|---
+ port|Integer|The port the server will listen on
+ https|Object|Settings for SSL used in `https.createServer`. Only the two required values below are allowed.
+ https.key|String|Path to the SSL key file
+ https.cert|String|Path to the SSL cert file
+ socketio|Object|Socket.IO server settings passed into `listen`. You are free to add whatever you need here.
+ redis|Object|Used to configure the redis client. Only the two required fields below should be used.
+ redis.client|Object|Passed into `redis.createClient`. Should be used for passing in auth, selecting db, or any other options allowed.
+ redis.prefix|String|Used to prefix all the keys.
+ 
---
+++ server.js
@@ -0,0 +1,87 @@
+var config = require('./config.json'),
+    https  = require('https'),
+    fs     = require('fs'),
+    io     = require('socket.io'),
+    redis  = require('redis'),
+    _      = require('lodash');
+
+var app = https.createServer({
+  key: fs.readFileSync(config.https.key),
+  cert: fs.readFileSync(config.https.cert)
+});
+io = io.listen(app, config.socketio);
+app.listen(config.port);
+
+var redisClient = redis.createClient(config.redis.client);
+redisClient.on("error", function(err) {
+  console.log("Error " + err);
+});
+
+var dibsIO = io.of('/dibs');
+dibsIO.on('connection', function(socket) {
+
+  socket.on('getAll', function(cb) {
+    redisClient.keys(makeKey('*'), function(err, keys) {
+      if (err) {
+        return cb(err);
+      }
+
+      var commands = _.map(keys, function(key) {
+        return ['hgetall', key];
+      });
+
+      redisClient.multi(commands)
+        .exec(function(err, rsp) {
+          if (err) {
+            return cb(err);
+          }
+
+          cb(null, rsp);
+        })
+    })
+  });
+
+  socket.on('claim', function(items, user, cb) {
+    var commands = [];
+
+    items.forEach(function(item) {
+      item.Name = user.DisplayName;
+      item.Full = true;
+      commands.push(['hmset', makeKey([item.EnvironmentId, item.ProjectId]), item]);
+    });
+
+    redisClient.multi(commands)
+      .exec(function(err, rsp) {
+        if (err) {
+          return cb(err);
+        }
+
+        dibsIO.emit('dibs.claimed', items);
+        cb(null, items);
+      });
+  });
+
+  socket.on('clear', function(items, user, cb) {
+    var keys = _.map(items, function(item) {
+      return makeKey([item.EnvironmentId, item.ProjectId]);
+    });
+
+    redisClient.del(keys, function(err, rsp) {
+      if (err) {
+        return cb(err);
+      }
+
+      dibsIO.emit('dibs.cleared', items);
+      cb(null, items);
+    });
+  });
+
+
+});
+
+function makeKey(parts) {
+  if (!_.isArray(parts)) {
+    parts = [parts];
+  }
+  return config.redis.prefix + '.' + parts.join('.');
+}
